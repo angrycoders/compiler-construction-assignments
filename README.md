0b08ea7dfe7b1b649953aa1b9965007a715a6617
GochoMugo <mugo@forfuture.co.ke>
2016-11-30 14:19:16 +0300

    script: Improve run.sh to handle solution directory

--- README.md
+++ README.md
@@ -15,13 +15,13 @@
 ```
 .
 ├── 1                       # Assignment I
-│   ├── 1_i                 # Answer for question 1.i
-│   ├── 1_ii                # Answer for question 1.ii
+│   ├── 1_i                 # Solution for question 1.i
+│   ├── 1_ii                # Solution for question 1.ii
 │   ├── 1_iii               # ...
 │   ├── 2                   # ...
 │   └── assignment.pdf      # The questions
 ├── 2                       # Assignment II
-│   ├── 1                   # Answer for question 1
+│   ├── 1                   # Solution for question 1
 │   ├── 2                   # ...
 │   ├── 3                   # ...
 │   └── assignment.pdf      # The questions
@@ -40,10 +40,22 @@ This allows compiling a parser and running it immediately.
 Usage:
 
 ```bash
-$ ./script/run.sh <solution.l> [solution.y] <input.txt>
+$ ./script/run.sh <path> [input-file]
 ```
 
-**Note**: `solution.y` is optional!
+where:
+
+* `<path>` is a path to a solution directory (**REQUIRED**)
+* `[input-file]` is a path to an input file,
+  or `-` to use stdin (**optional**)
+
+For example,
+
+```bash
+$ ./script/run.sh 1/2               # run solution at 1/2
+$ ./script/run.sh 1/2 input.txt     # run solution at 1/2, using a custom input file
+$ ./script/run.sh 2/1 -             # run solution at 2/1, using stdin
+```
 
 
 ## license:
--- script/run.sh
+++ script/run.sh
@@ -1,67 +1,120 @@
 #!/usr/bin/env bash
 
-set -e
 
-lex_file="$(readlink -f "${1}")"
-yacc_file=""
-input_file="$(readlink -f "${2}")"
-lex_file_id="$(sed -e s/\\W/_/g <<< "${lex_file}")"
-tmp_dir="/tmp/${lex_file_id}"
-
+base_dir="$(readlink -f "${1}")"
+arg_input_file="${2}"
+solution_id="$(sed -e s/\\W/_/g <<< "${base_dir}")"
+tmp_dir="/tmp/compiler-assignments/${solution_id}"
+# input files
+lex_file="${base_dir}/solution.l"
+yacc_file="${base_dir}/solution.y"
+c_file="${base_dir}/solution.c"
+input_file="${base_dir}/input.txt"
 # colors
+COLOR_RED="\033[0;31m"
 COLOR_RESET="\e[0m"
 COLOR_WHITE="\033[1;37m"
+# intermediate output
+c_files=""
+in_file=""
 
 
-# logging to console
+# Log to stdout, with colors
+# @param message
 log() {
-    echo -e "${COLOR_WHITE}${color}${1}${COLOR_RESET}"
+    echo -e "${COLOR_WHITE}${1}${COLOR_RESET}"
 }
 
 
-# optional yacc spec file
-if [[ -n "${3}" ]]
-then
-    yacc_file="${input_file}"
-    input_file="$(readlink -f "${3}")"
-fi
+# Log to stderr, with colors
+# @param message
+error() {
+    echo -e "${COLOR_RED}ERROR: ${1}${COLOR_RESET}" 1>&2
+}
 
-# we need a lex file
-if [[ -z "${lex_file}" ]]
+
+# we require path to the directory
+if [[ ! -d "${base_dir}" ]]
 then
-    echo "Lex file missing"
+    error "missing solution directory"
     exit 1
 fi
 
-# we need an input file
-if [[ -z "${input_file}" ]]
+
+# we allow passing a custom input file
+if [[ -n "${arg_input_file}" ]]
 then
-    echo "Input file missing"
-    exit 1
+    # '-' means use stdin
+    if [[ "${arg_input_file}" == "-" ]]
+    then
+        input_file=""
+    else
+        input_file="$(readlink -f "${arg_input_file}")"
+    fi
 fi
 
+
+# enter our temporary directory
 mkdir -p "${tmp_dir}"
-pushd "${tmp_dir}" > /dev/null || {
-    echo "Can NOT cd into tmp dir"
+pushd "${tmp_dir}" > /dev/null
+if [[ ! $? ]]
+then
+    error "Can not cd into temporary directory"
     exit 1
-}
+fi
 
 
-c_files="lex.yy.c"
-
-
-if [[ -n "${yacc_file}" ]]
+# process yacc spec file
+if [[ -f "${yacc_file}" ]]
 then
     log ">> yacc"
-    yacc -d "${yacc_file}"
+    yacc -d "${yacc_file}" || exit 1
     c_files="${c_files} y.tab.c"
 fi
 
-log ">> lex"
-lex "${lex_file}"
 
+# process lex spec file
+if [[ -f "${lex_file}" ]]
+then
+    log ">> lex"
+    lex "${lex_file}" || exit 1
+    c_files="${c_files} lex.yy.c"
+fi
+
+
+# capture c file
+if [[ -f "${c_file}" ]]
+then
+    c_files="${c_files} ${c_file}"
+fi
+
+
+# ensure we have files to compile
+if [[ -z "${c_files}" ]]
+then
+    error "nothing to do"
+    error "ensure you provided a solution directory"
+    exit 1
+fi
+
+
+# compile
 log ">> gcc"
-gcc ${c_files} -ll
+gcc ${c_files} -ll || exit 1
 
+
+# un-capture input file
+if [[ -f "${input_file}" ]]
+then
+    in_file="${input_file}"
+# we exit with an error if our custom input file is missing
+elif [[ -n "${arg_input_file}" ]]
+then
+    error "custom input file is missing"
+    exit 1
+fi
+
+
+# run the executable
 log ">> run"
-./a.out "${input_file}"
+./a.out ${in_file}
